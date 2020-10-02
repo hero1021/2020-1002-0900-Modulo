ec25bb4971e53c58e11e8153c8ea42444d3a9db4
hero1021 <herojun1021@gmail.com>
2020-10-02 12:13:14 -0700

    master

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
 my_team = 27 % 4
 
 print(my_team)
+
+
+def sing_song():
+    print("You may say I'm a dreamer")
+    print("But I'm not the only one")
+    print("I hope some day you'll join us")
+    print("And the world will be as one")
+
+# call sing_song() below:
